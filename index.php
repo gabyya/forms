421958ba85c81a48c4f319967d81e4e11464a2a6
gabyya <31488533+gabyya@users.noreply.github.com>
2017-10-02 17:44:53 -0700

    added PHP switch

--- index.php
+++ index.php
@@ -10,26 +10,68 @@
       <h1>HTML Forms &mdash; Get</h1>
       <form action="results.php" method="post">
         <div class="form-group">
-          <label for="first-name">First Name</label>
-          <input type="text" class="form-control" id="first-name" aria-describedby="first-name-help" placeholder="First Name" name="first_name">
-          <small id="first-name-help" class="form-text text-muted">Enter your first name.</small>
+          <label for="nickname">Nickname</label>
+          <input type="text" class="form-control" id="first-name" aria-describedby="email-help" placeholder="Nickname" name="nickname">
+          <small id="nickname-help" class="form-text text-muted">Enter your nickname.</small>
         </div>
         <div class="form-group">
-          <label for="last-name">Last Name</label>
-          <input type="text" class="form-control" id="last-name" aria-describedby="last-name-help" placeholder="Last Name" name="last_name">
-          <small id="last-name-help" class="form-text text-muted">Enter your last name.</small>
+          <label for="last-name">First and Last Name</label>
+          <input type="text" class="form-control" id="last-name" aria-describedby="first-and-last-name-help" placeholder="First and Last Name" name="first_and_last_name">
+          <small id="first_and_last-name-help" class="form-text text-muted">Enter your first and last name.</small>
         </div>
-        <p class="lead mb-0">Would you rather have a cat or a dog?</p>
+        <p class="lead mb-0">What is your favorite movie genre?</p>
         <div class="form-check">
           <label class="form-check-label">
-            <input class="form-check-input" type="radio" name="animals" id="cats" value="cat">
-            Cat
+            <input class="form-check-input" type="radio" name="movies" id="horror" value="horror">
+            Horror
           </label>
         </div>
         <div class="form-check">
           <label class="form-check-label">
-            <input class="form-check-input" type="radio" name="animals" id="dogs" value="dog">
-            Dog
+            <input class="form-check-input" type="radio" name="movies" id="comedy" value="comedy">
+            Comedy
+          </label>
+        </div>
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="movies" id="romantic" value="romantic">
+            Romantic
+          </label>
+        </div>
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="movies" id="science_fiction" value="science_fiction">
+            Science Fiction
+          </label>
+        </div>
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="movies" id="suspense" value="suspense">
+            Suspense
+          </label>
+        </div>
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="movies" id="indie" value="indie">
+            Indie
+          </label>
+        </div>
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="movies" id="chick_flick" value="chick_flick">
+            Chick Flick
+          </label>
+        </div>
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="movies" id="action" value="action">
+            Action
+          </label>
+        </div>
+        <div class="form-check">
+          <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="movies" id="none" value="none">
+            None of the above
           </label>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
--- results.php
+++ results.php
@@ -6,20 +6,61 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
   </head>
   <body>
+
+    <?php
+    
+      switch ($_POST["movies"]) {
+        case "horror":
+          $comment =
+           "You're a horror movie lover.";
+          break;
+           case "comedy":
+          $comment =
+           "You're a comedy movie lover.";
+          break;
+           case "romantic":
+          $comment =
+          "You're a romantic movie lover.";
+          break;
+           case "science_fiction":
+          $comment = "You're a science fiction movie lover.";
+          break;
+           case "suspense":
+          $comment = "You're a suspense movie lover.";
+          break;
+           case "indie":
+          $comment = "You're an indie movie lover.";
+          break;
+           case "chick_flick":
+          $comment = "You're a chick flick movie lover.";
+          break;
+           case "action":
+          $comment = "You're an action movie lover.";
+          break;
+           case "none":
+          $comment = "You have horrible taste.";
+          break;
+          default:
+            $comment = "You&rsquo;re a 
+            <?php echo $movies; ?> movie lover.";
+      }
+      ?>
+
+
     <?php
       // Set variables
-      $first_name = htmlspecialchars($_POST["first_name"]);
-      $last_name = htmlspecialchars($_POST["last_name"]);
-      $animals = htmlspecialchars($_POST["animals"]);
+      $nickname = htmlspecialchars($_POST["nickname"]);
+      $first_and_last_name = htmlspecialchars($_POST["first_and_last_name"]);
+      //$movies = htmlspecialchars($_POST["movies"]);
     ?>
     <div class="container py-5 my-5">
       <pre>
       </pre>
-      <h1>Hello, <?php echo $first_name.' '.$last_name; ?></h1>
-      <?php if($animals != null): ?>
-        <p class="lead">You&rsquo;re a <?php echo $animals; ?> person.</p>
+      <h1>Sup, <?php echo $nickname; ?></h1>
+      <?php if($_POST["movies"] != null): ?>
+        <p class="lead"><?php echo $comment; ?></p>
       <?php else: ?>
-        <p>Enter an animal.</p>
+        <p>Enter a movie.</p>
       <?php endif; ?>
     </div>
   </body>
